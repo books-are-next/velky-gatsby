ed8f958bc80d543d13be84684fa6b02cc39f794e
jan-martinek <honza.martinek@gmail.com>
2021-12-01 15:57:44 +0000

    Deploying to docs from @ books-are-next/velky-gatsby@eeefeab03805d876658fa89428e44a3db2a617e8 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-87b16d2';
+const CACHE = 'cache-eeefeab';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -39,7 +39,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./resources.html","./velky_getsby_001.html","./velky_getsby_002.html","./manifest.json","./velky_getsby_007.html","./velky_getsby_008.html","./velky_getsby_009.html","./velky_getsby_010.html","./velky_getsby_011.html","./velky_getsby_012.html","./velky_getsby_013.html","./velky_getsby_014.html","./velky_getsby_015.html","./resources/image001_fmt.png","./resources/image002_fmt.png","./resources/index.xml","./resources/obalka_velky_gatsby_fmt.png","./resources/upoutavka_eknihy_fmt.png","./scripts/bundle.js","./style/style.min.css"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./resources.html","./velky_getsby_001.html","./velky_getsby_002.html","./velky_getsby_007.html","./velky_getsby_009.html","./velky_getsby_008.html","./velky_getsby_010.html","./velky_getsby_011.html","./velky_getsby_012.html","./velky_getsby_013.html","./velky_getsby_014.html","./velky_getsby_015.html","./resources/image001_fmt.png","./resources/image002_fmt.png","./resources/index.xml","./resources/obalka_velky_gatsby_fmt.png","./resources/upoutavka_eknihy_fmt.png","./scripts/bundle.js","./style/style.min.css"]));
 }
 
 self.addEventListener('fetch', e => {
